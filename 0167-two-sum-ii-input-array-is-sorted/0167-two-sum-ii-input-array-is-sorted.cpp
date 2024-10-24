0bdc74ea679708fd8af7d351f12c788b9035f851
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-10-24 22:15:02 +0600

    Time: 7 ms (77.76%), Space: 18.4 MB (15.39%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,24 +1,15 @@
 // say Alhamdulillah
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-using namespace __gnu_pbds;
-#define ll long long
-
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    indexed_multiset;
 
 class Solution {
 public:
   vector<int> twoSum(vector<int> &numbers, int target) {
-    indexed_multiset st;
     for (int i = 0; i < numbers.size(); i++) {
       int idx =
-          upper_bound(numbers.begin(), numbers.end(), target - numbers[i]) -
+          upper_bound(numbers.begin()+i, numbers.end(), target - numbers[i]) -
           numbers.begin() - 1;
-      cout << i << " --- " << idx << endl;
       if (idx < numbers.size() && i != idx &&
           numbers[i] + numbers[idx] == target) {
         return {i + 1, idx + 1};

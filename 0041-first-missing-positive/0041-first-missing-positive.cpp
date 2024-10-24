b4564cf3b92a7e398d01a3cc145feb25b6b7c3b7
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-10-24 15:25:59 +0600

    Time: 0 ms (100%), Space: 53.8 MB (81.65%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,19 +1,12 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-using namespace __gnu_pbds;
-#define ll long long
-
-typedef tree<pair<ll, char>, null_type, greater<pair<ll, char>>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    indexed_multiset;
 
 class Solution {
 public:
   int firstMissingPositive(vector<int> &nums) {
-    int mini = INT_MAX;
+    int i = 0;
 
-    for (int i = 0; i < nums.size(); i++) {
+    for (i = 0; i < nums.size(); i++) {
       while (1 <= nums[i] && nums[i] <= nums.size() && nums[i] != i + 1 &&
              nums[i] != nums[nums[i] - 1])
         swap(nums[i], nums[nums[i] - 1]);

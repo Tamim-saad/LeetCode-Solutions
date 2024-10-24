08cfed510d11f2f9d186a18a4e0c389888d39865
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-10-25 02:06:31 +0600

    Time: 60 ms (94.78%), Space: 93.7 MB (48.67%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,16 +1,14 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
   int eraseOverlapIntervals(vector<vector<int>> &intervals) {
-    if (intervals.size() <= 1) {
-      return 0;
-    }
+
     sort(intervals.begin(), intervals.end(),
          [](const vector<int> &a, const vector<int> &b) {
-             return a[1] < b[1];
-        //    if (a[1] == b[1])
+           return a[1] < b[1];
          });
 
     // for (int i = 0; i < intervals.size(); i++)

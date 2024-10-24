ab616056acd9f0443e8ac86c281ade1a06729685
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-10-24 22:15:45 +0600

    Time: 1 ms (93.27%), Space: 18.3 MB (15.39%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -6,11 +6,12 @@ using namespace std;
 class Solution {
 public:
   vector<int> twoSum(vector<int> &numbers, int target) {
-    for (int i = 0; i < numbers.size(); i++) {
+    int n=numbers.size();
+    for (int i = 0; i < n; i++) {
       int idx =
           upper_bound(numbers.begin()+i, numbers.end(), target - numbers[i]) -
           numbers.begin() - 1;
-      if (idx < numbers.size() && i != idx &&
+      if (idx < n && i != idx &&
           numbers[i] + numbers[idx] == target) {
         return {i + 1, idx + 1};
       }

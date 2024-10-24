9cd5be337937a8bee197bc7b4f2e705bac4f889e
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-10-24 21:31:16 +0600

    Time: 239 ms (5.83%), Space: 27.6 MB (81.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
   void rotate(vector<int> &nums, int k) {
-    if (k == 0)
-      return;
     if (k >= nums.size())
       k %= nums.size();
     reverse(nums.begin(), nums.end());
-    int i, j;
-    i = 0;
-    j = k - 1;
-    while (i < j) {
-      swap(nums[i++], nums[j--]);
-    }
-    i = k;
-    j = nums.size() - 1;
-    while (i < j) {
-      {
-        swap(nums[i++], nums[j--]);
-      }
-    }
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
     for (auto x : nums)
       cout << x << " ";
     cout << endl;

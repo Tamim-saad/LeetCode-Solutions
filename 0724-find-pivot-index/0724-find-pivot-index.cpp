584dee49e4bfd31884cec08ba72fc9442c67f0c0
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-10-25 20:08:24 +0600

    Time: 390 ms (11.94%), Space: 34.6 MB (68.19%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+  int pivotIndex(vector<int> &nums) {
+    for (int i = 1; i < nums.size(); i++) {
+      nums[i] = nums[i] + nums[i - 1];
+    }
+    int l = 0;
+    int r = 0;
+    int x;
+    for (int i = 0; i < nums.size(); i++) {
+
+      x = (i == 0 ? nums[i] : nums[i] - nums[i - 1]);
+      l = (i == 0 ? 0 : nums[i - 1]);
+      r = (i == nums.size() - 1 ? 0 : nums[nums.size() - 1] - nums[i]);
+      cout << x << " " << l << " " << r << endl;
+      if (l == r)
+        return i;
+    }
+    return -1;
+  }
+};

732105faca5beadb7ca140e48f9043ef8679fbd1
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-10-25 19:24:58 +0600

    Time: 6 ms (23.8%), Space: 9.7 MB (5.15%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  string mergeAlternately(string word1, string word2) {
+    string s = "";
+    int i = 0, j = 0;
+    while (i < word1.length() && j < word2.length()) {
+      s = s + word1[i++];
+      s = s + word2[j++];
+    }
+    while (i < word1.length())
+      s = s + word1[i++];
+    while (j < word2.length())
+      s = s + word2[j++];
+    return s;
+  }
+};

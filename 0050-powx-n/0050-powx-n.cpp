7e2b5de4a524bdd82f3a6dc8fdbe5028e03856b1
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-12-28 23:12:40 +0600

    Time: 0 ms (100%), Space: 8.5 MB (41.35%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -6,17 +6,12 @@ using namespace std;
 class Solution {
 public:
   double myPow(long double x, long long n) {
-    // if (n == 0)
-    //   return 1;
-    // if (x == 0)
-    //   return 0;
-
-    double ans = 1.0;
     if (n < 0) {
       x = 1 / x;
       n *= -1;
     }
 
+    double ans = 1.0;
     while (n) {
       if (n & 1)
         ans *= x;

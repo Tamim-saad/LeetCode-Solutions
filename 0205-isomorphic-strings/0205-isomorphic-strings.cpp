e44c22daa3a5e0d69669a6bbe193dcbbb3929787
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-12-28 16:14:21 +0600

    Time: 0 ms (100%), Space: 9.2 MB (82.93%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -6,16 +6,12 @@ using namespace std;
 class Solution {
 public:
   bool isIsomorphic(string s, string t) {
-    // map<char, char> mps, mpt;
 
     vector<char> mps(256, 0), mpt(256, 0);
 
     for (int i = 0; i < s.size(); i++) {
       if (mps[s[i]] == 0)
         mps[s[i]] = t[i];
-      // if (mps.find(s[i]) == mps.end()) {
-      //   mps.insert({s[i], t[i]});
-      // }
 
       if (mps[s[i]] != t[i])
         return false;
@@ -23,9 +19,6 @@ public:
     for (int i = 0; i < t.size(); i++) {
       if (mpt[t[i]] == 0)
         mpt[t[i]] = s[i];
-      // if (mpt.find(t[i]) == mpt.end()) {
-      //   mpt.insert({t[i], s[i]});
-      // }
 
       if (mpt[t[i]] != s[i])
         return false;

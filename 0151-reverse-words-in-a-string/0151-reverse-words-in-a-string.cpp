5dc9dc6015143a5b2875600124af3f4fe61b4dd1
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-12-28 15:14:09 +0600

    Time: 0 ms (100%), Space: 11.1 MB (29.85%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,10 +1,3 @@
-// #include <ext/pb_ds/assoc_container.hpp>
-// #include <ext/pb_ds/tree_policy.hpp>
-// using namespace __gnu_pbds;
-// typedef tree<tuple<int, int, int>, null_type, less<tuple<int, int, int>>,
-//              rb_tree_tag, tree_order_statistics_node_update>
-//     indexed_multiset;
-
 // say Alhamdulillah
 
 #include <bits/stdc++.h>
@@ -14,7 +7,7 @@ class Solution {
 public:
   string reverseWords(string s) {
 
-    int ans = 0, i, j;
+    int i, j;
     vector<string> v;
 
     for (i = 0; i < s.size(); i++) {
@@ -25,7 +18,6 @@ public:
 
       while (i < s.size() && s[i] != 32) {
         t += s[i];
-        ans++;
         i++;
       }
       t += ' ';
@@ -34,16 +26,14 @@ public:
     reverse(v.begin(), v.end());
     string rs;
 
-    for (i = 0; i < v.size(); i++) {
+    for (i = 0; i < v.size(); i++)
       for (j = 0; j < v[i].size(); j++)
         rs += v[i][j];
-    }
 
     while (!rs.empty() && rs[0] == 32)
       rs.erase(rs.begin());
 
-    // while (!rs.empty() && rs[rs.size() - 1] == 32)
-      rs.erase(rs.size() - 1);
+    rs.erase(rs.size() - 1);
     return rs;
   }
 };

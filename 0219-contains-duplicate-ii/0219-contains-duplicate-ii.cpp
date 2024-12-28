7a3bec2fa8feb200c88b4205b4d2ba7560bb7b86
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-12-28 16:48:38 +0600

    Time: 284 ms (11.79%), Space: 126.6 MB (5%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,38 @@
+
+// say Alhamdulillah
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  bool containsNearbyDuplicate(vector<int> &nums, int k) {
+
+    map<int, vector<int>> mp;
+
+    for (int i = 0; i < nums.size(); i++) {
+      mp[nums[i]].emplace_back(i);
+    }
+
+    vector<int> dup = nums;
+
+    sort(nums.begin(), nums.end());
+    unique(nums.begin(), nums.end());
+
+    bool f = false;
+    for (auto x : nums) {
+      vector<int> v = mp[x];
+      for (int i = 1; i < v.size(); i++) {
+        if (abs(v[i] - v[i - 1]) <= k && dup[v[i]] == dup[v[i - 1]]) {
+          f = true;
+          break;
+        }
+        if (abs(v[i] - v[i - 1]) > k)
+          continue;
+      }
+      if (f)
+        break;
+    }
+    return f;
+  }
+};

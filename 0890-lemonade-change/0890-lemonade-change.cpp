336acdad8b8a0e7a89511336757d69b10f69c70a
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-12-31 16:42:52 +0600

    Time: 0 ms (100%), Space: 87.2 MB (24.33%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,4 +1,3 @@
-
 // say Alhamdulillah
 
 #include <bits/stdc++.h>
@@ -8,25 +7,23 @@ class Solution {
 public:
   bool lemonadeChange(vector<int> &bills) {
     int n5 = 0, n10 = 0;
-    int ex5 = 0, ex10 = 0, ex20 = 0;
 
-    // 5 5 5 10 20
     for (auto x : bills) {
-      if (x == 5) {
-        ex5++; // 8
-      }
+      if (x == 5)
+        n5++;
+
       if (x == 10) {
-        ex5--; // 0
-        ex10++;
-        if (ex5 < 0)
+        n5--;
+        n10++;
+        if (n5 < 0)
           return false;
       }
       if (x == 20) {
-        if ((ex10 >= 1 && ex5 >= 1)) {
-          ex10--;
-          ex5--;
-        } else if (ex5 >= 3) {
-          ex5 -= 3;
+        if ((n10 >= 1 && n5 >= 1)) {
+          n10--;
+          n5--;
+        } else if (n5 >= 3) {
+          n5 -= 3;
         } else
           return false;
       }

507b05a87898caf9a5ed3eb30eac9dff04f743b7
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-12-31 08:40:50 +0600

    Time: 0 ms (100%), Space: 7.7 MB (36.48%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,19 @@
+// say Alhamdulillah
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  string convertToTitle(int columnNumber) {
+    string ans = "";
+    while (columnNumber > 0) {
+      columnNumber--;
+      ans += 'A' + columnNumber % 26;
+      columnNumber /= 26;
+    }
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+  }
+};

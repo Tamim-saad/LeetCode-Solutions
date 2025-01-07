a5d7d2889543cb0a5af98daa36b5462d21641da4
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2025-01-07 13:44:55 +0600

    Time: 5 ms (56.1%), Space: 23.9 MB (47.82%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,19 +6,15 @@ using namespace std;
 class Solution {
 public:
   vector<vector<int>> merge(vector<vector<int>> &intervals) {
+
     sort(intervals.begin(), intervals.end());
-
     vector<vector<int>> vans;
-    vans.push_back(intervals[0]);
 
-    int i = 1;
-    while (i < intervals.size()) {
-      if (vans[vans.size() - 1][1] >= intervals[i][0])
-        vans[vans.size() - 1][1] =
-            max(vans[vans.size() - 1][1], intervals[i][1]);
+    for (int i = 0; i < intervals.size(); i++) {
+      if (!i || vans.back()[1] < intervals[i][0])
+        vans.emplace_back(intervals[i]);
       else
-        vans.push_back({intervals[i][0], intervals[i][1]});
-      i++;
+        vans.back()[1] = max(vans.back()[1], intervals[i][1]);
     }
 
     return vans;

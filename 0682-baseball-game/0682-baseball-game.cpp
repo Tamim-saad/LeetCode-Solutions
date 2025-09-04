cb684bb1a57e5f173f2a293f84edbdffa50f0b36
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2025-09-04 19:43:24 +0600

    Time: 0 ms (100%), Space: 12.8 MB (41.31%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,36 @@
+/// Alhamdulillah
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+  int calPoints(vector<string> &operations) {
+    string s, t, x;
+    stack<int> st;
+
+    for (auto x : operations) {
+      if (x == "+") {
+        int t1 = st.top();
+        st.pop();
+        int t2 = st.top();
+        st.push(t1);
+        st.push(t1 + t2);
+      } else if (x == "D") {
+        st.push(2 * st.top());
+      } else if (x == "C") {
+        st.pop();
+      } else {
+        st.push(stoi(x));
+      }
+    }
+
+    int ans = 0;
+    while (!st.empty()) {
+      ans += st.top();
+      st.pop();
+    }
+    return ans;
+  }
+};

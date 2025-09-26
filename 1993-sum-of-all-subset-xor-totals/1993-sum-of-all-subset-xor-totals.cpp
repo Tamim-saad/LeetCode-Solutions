65042658f317380174d358f90fe91ab15e97793e
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2025-09-26 17:41:30 +0600

    Time: 0 ms (100%), Space: 9.1 MB (68.17%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -3,12 +3,25 @@ using namespace std;
 
 class Solution {
 public:
-  int backtrack(int i, int xr, vector<int> nums) {
-    if (i >= nums.size())
-      return xr;
+  int subsetXORSum(vector<int> &nums) {
+    int xr = 0;
+    for (auto x : nums) {
+      xr |= x;
+    }
 
-    return backtrack(i + 1, xr ^ nums[i], nums) + backtrack(i + 1, xr, nums);
+    return xr * pow(2, nums.size() - 1);
   }
-
-  int subsetXORSum(vector<int> &nums) { return backtrack(0, 0, nums); }
 };
+
+// Another Solution
+//  class Solution {
+//  public:
+//    int backtrack(int i, int xr, vector<int> nums) {
+//      if (i >= nums.size())
+//        return xr;
+
+//     return backtrack(i + 1, xr ^ nums[i], nums) + backtrack(i + 1, xr, nums);
+//   }
+
+//   int subsetXORSum(vector<int> &nums) { return backtrack(0, 0, nums); }
+// };

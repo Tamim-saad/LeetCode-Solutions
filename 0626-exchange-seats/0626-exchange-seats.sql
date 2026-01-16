b47b3b00f2f2ee75f31da008e9d1e3ff7670988d
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-01-16 09:21:14 +0600

    Time: 316 ms (75.96%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select o.id, if(o.id % 2 = 0,
-    (select s.student from Seat s where s.id+1=o.id),
-    ifnull((select s.student from Seat s where s.id-1=o.id), o.student))  as student
+    (select s.student from Seat s where s.id=o.id-1),
+    ifnull((select s.student from Seat s where s.id=o.id+1), o.student))  as student
 from Seat o
 order by o.id

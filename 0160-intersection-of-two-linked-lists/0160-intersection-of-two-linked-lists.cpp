90a7778a0bf207b8619590fe7db048dd5069cd8a
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-01-29 21:15:08 +0600

    Time: 108 ms (7.78%), Space: 35.2 MB (5.41%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,20 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    map<ListNode *, bool> mp;
+    while (headA) {
+      mp[headA] = true;
+      headA = headA->next;
+    }
+    while (headB) {
+      if (mp[headB])
+        return headB;
+      headB = headB->next;
+    }
+    return nullptr;
+  }
+};

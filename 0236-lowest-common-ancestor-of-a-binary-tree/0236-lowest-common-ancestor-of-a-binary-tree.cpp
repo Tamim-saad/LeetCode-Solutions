1f7fa1323f5926bac02742e64c685116127a7eac
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-01-29 21:02:38 +0600

    Time: 8 ms (90.01%), Space: 17.2 MB (87.22%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -3,33 +3,16 @@ using namespace std;
 
 class Solution {
 public:
-  map<TreeNode *, bool> mp, mp2;
-
-  bool recur(TreeNode *root, TreeNode *x, map<TreeNode *, bool> &mp) {
-    if (!root)
-      return false;
-    if (root->val == x->val)
-      return mp[root] = true;
-
-    return mp[root] = recur(root->left, x, mp) || recur(root->right, x, mp);
-  }
-
-  TreeNode *search(TreeNode *root) {
-    auto x = root->val;
-
-    if (root->left && mp[root->left] && mp2[root->left]) {
-      return search(root->left);
-    } else if (root->right && mp[root->right] && mp2[root->right]) {
-      return search(root->right);
-    } else {
-      return root;
-    }
-  }
-
   TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
-    recur(root, p, mp);
-    recur(root, q, mp2);
+    if (!root || root == p || root == q)
+      return root;
 
-    return search(root);
+    auto left = lowestCommonAncestor(root->left, p, q);
+    auto right = lowestCommonAncestor(root->right, p, q);
+
+    if (left && right)
+      return root;
+
+    return left ? left : right;
   }
 };

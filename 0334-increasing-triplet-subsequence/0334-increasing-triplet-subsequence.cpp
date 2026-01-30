c254f0b1cb25de635329b826c3322c18ef1d8012
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-01-31 02:23:44 +0600

    Time: 12 ms (5.46%), Space: 115.6 MB (90.22%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  bool increasingTriplet(vector<int> &nums) {
+    vector<int> seq;
+    seq.push_back(nums[0]);
+
+    for (int i = 1; i < nums.size(); i++) {
+      if (seq.back() < nums[i])
+        seq.push_back(nums[i]);
+      else {
+        int ind = lower_bound(seq.begin(), seq.end(), nums[i]) - seq.begin();
+        seq[ind] = nums[i];
+      }
+    }
+    
+    return seq.size() >= 3;
+  }
+};

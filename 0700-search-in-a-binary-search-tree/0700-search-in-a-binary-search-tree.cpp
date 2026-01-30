1aa2cbcb1202c9f3d0ba961be4643e11a40f48a2
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-01-30 15:14:54 +0600

    Time: 0 ms (100%), Space: 35.3 MB (92.95%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -0,0 +1,20 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  TreeNode *searchBST(TreeNode *root, int val) {
+    if (!root)
+      return nullptr;
+    if (val == root->val) {
+      return root;
+    }
+    if (val > root->val)
+      return searchBST(root->right, val);
+    if (val < root->val)
+      return searchBST(root->left, val);
+
+    return nullptr;
+  }
+};

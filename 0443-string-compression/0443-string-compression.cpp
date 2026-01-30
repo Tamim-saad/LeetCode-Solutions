839915db480ca09cc9fce76236db7a59486193bc
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-01-31 02:06:51 +0600

    Time: 0 ms (100%), Space: 14.7 MB (13.16%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -5,29 +5,30 @@ using namespace std;
 class Solution {
 public:
   void modify(vector<char> &chars, int val, int &ind) {
+    if (val == 1)
+      return;
+
     auto s = to_string(val);
-    for (int i = 0; i < s.size(); i++) {
+    for (int i = 0; i < s.size(); i++)
       chars[ind++] = s[i];
-    }
   }
+
   int compress(vector<char> &chars) {
     int curr = 1, i = 0, j = 1, ind = 0;
 
     while (j < chars.size()) {
-      if (chars[j] == chars[i]) {
+      if (chars[j] == chars[i])
         curr++;
-      } else {
+      else {
         chars[ind++] = chars[j - 1];
-        if (curr > 1)
-          modify(chars, curr, ind);
+        modify(chars, curr, ind);
         curr = 1;
         i = j;
       }
       j++;
     }
     chars[ind++] = chars[j - 1];
-    if (curr > 1)
-      modify(chars, curr, ind);
+    modify(chars, curr, ind);
 
     return ind;
   }

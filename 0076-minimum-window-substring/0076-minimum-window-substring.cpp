9cce63f3290dc8c9a6ab9553c098195b4a8a8fe5
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-02-10 15:05:18 +0600

    Time: 22 ms (16.55%), Space: 12.3 MB (5.5%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,36 +6,40 @@ public:
   string minWindow(string s, string t) {
     string ans = "";
     map<char, int> pattern, mp;
-    int target = 0, cnt = 0;
-    for (auto x : t) {
+    int n = t.size(), cnt = 0;
+
+    for (auto x : t)
       pattern[x]++;
-      target++;
-    }
 
     int i = 0, j = 0;
     while (i < s.size()) {
-      auto c = s[i];
-      if (!pattern.count(c)) {
-        i++;
-        continue;
-      }
+      // if (pattern.count(s[i]) == 0) {
+      //   i++;
+      //   continue;
+      // }
 
-      mp[c]++;
-      if (mp[c] <= pattern[c])
+      if (mp[s[i]] < pattern[s[i]])
         cnt++;
+      mp[s[i]]++;
 
-      while (j <= i) {
-        if (!pattern.count(s[j])) {
-          j++;
-        } else if (mp[s[j]] > pattern[s[j]]) {
-          mp[s[j]]--;
-          j++;
-        } else {
-          break;
-        }
+      while (j <= i && mp[s[j]] > pattern[s[j]]) {
+        mp[s[j]]--;
+        j++;
       }
 
-      if (cnt == target && (ans.empty() || i - j + 1 < ans.length()))
+      // while (j <= i) {
+      //   // if (!pattern.count(s[j])) {
+      //   //   j++;
+      //   // } else
+      //   if (mp[s[j]] > pattern[s[j]]) {
+      //     mp[s[j]]--;
+      //   } else {
+      //     break;
+      //   }
+      //   j++;
+      // }
+
+      if (cnt == n && (ans.empty() || i - j + 1 < ans.length()))
         ans = s.substr(j, i - j + 1);
 
       i++;

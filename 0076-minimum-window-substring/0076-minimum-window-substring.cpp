83a9a361d5d6259c0105dd92e9a96247e6581204
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-02-11 11:30:31 +0600

    Time: 23 ms (15.65%), Space: 12.3 MB (5.51%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -4,40 +4,24 @@ using namespace std;
 class Solution {
 public:
   string minWindow(string s, string t) {
-    string ans = "";
     map<char, int> pattern, mp;
-    int n = t.size(), cnt = 0;
 
     for (auto x : t)
       pattern[x]++;
 
-    int i = 0, j = 0;
-    while (i < s.size()) {
-      // if (pattern.count(s[i]) == 0) {
-      //   i++;
-      //   continue;
-      // }
+    int i = 0, j = 0, cnt = 0, n = t.size();
+    string ans = "";
 
-      if (mp[s[i]] < pattern[s[i]])
-        cnt++;
+    while (i < s.size()) {
       mp[s[i]]++;
+
+      if (mp[s[i]] <= pattern[s[i]])
+        cnt++;
 
       while (j <= i && mp[s[j]] > pattern[s[j]]) {
         mp[s[j]]--;
         j++;
       }
-
-      // while (j <= i) {
-      //   // if (!pattern.count(s[j])) {
-      //   //   j++;
-      //   // } else
-      //   if (mp[s[j]] > pattern[s[j]]) {
-      //     mp[s[j]]--;
-      //   } else {
-      //     break;
-      //   }
-      //   j++;
-      // }
 
       if (cnt == n && (ans.empty() || i - j + 1 < ans.length()))
         ans = s.substr(j, i - j + 1);

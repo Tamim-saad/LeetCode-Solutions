79274fc8857691ceb8a34017e797577e0ff3c3aa
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-02-17 20:57:07 +0600

    Time: 15 ms (21.13%), Space: 41 MB (7.34%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  vector<vector<int>> ans;
+  void dfs(TreeNode *root, vector<int> curr, int sum, int target) {
+    if (!root)
+      return;
+
+    curr.emplace_back(root->val);
+    sum += root->val;
+
+    if (!root->left && !root->right && sum == target) {
+      ans.emplace_back(curr);
+    }
+
+    dfs(root->left, curr, sum, target);
+    dfs(root->right, curr, sum, target);
+    // curr.pop_back();
+    // sum -= root->val;
+  }
+
+  vector<vector<int>> pathSum(TreeNode *root, int targetSum) {
+
+    dfs(root, {}, 0, targetSum);
+    return ans;
+  }
+};

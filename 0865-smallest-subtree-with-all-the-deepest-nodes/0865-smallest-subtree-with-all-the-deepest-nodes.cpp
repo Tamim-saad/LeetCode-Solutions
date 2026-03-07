9d9773219b9c5b96016b1eba4a518aff431823d2
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-03-07 19:48:17 +0600

    Time: 0 ms (100%), Space: 16.4 MB (34.32%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -1,51 +1,38 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
 class Solution {
 public:
-  int maxi = 0;
-  map<int, int> mp;
+  TreeNode *ans = nullptr;
+  int maxLevel(TreeNode *root) {
+    if (!root)
+      return 0;
 
-  int searchMax(TreeNode *root, int level) {
-    if (!root) return 0;
-    level += 1;
-    mp[root->val] = level;
-    maxi = max(level, maxi);
-    searchMax(root->left, level);
-    searchMax(root->right, level);
-    return mp[root->val];
+    return max(maxLevel(root->left), maxLevel(root->right)) + 1;
   }
 
-  TreeNode *lowestRoot = nullptr;
+  int recur(TreeNode *root, int currlevel, int &maxLev) {
+    if (!root)
+      return currlevel;
 
-  // minimal change: traverse now returns the maximum depth found in this subtree
-  int traverse(TreeNode *root) {
-    if (!root) return 0;
+    currlevel++;
 
-    int leftMax = traverse(root->left);
-    int rightMax = traverse(root->right);
+    int l = recur(root->left, currlevel, maxLev);
+    int r = recur(root->right, currlevel, maxLev);
 
-    int curr = mp[root->val];
-    int subtreeMax = max(curr, max(leftMax, rightMax));
+    if ((l == maxLev && r == maxLev) ||
+        (l == 0 && r == 0 && currlevel == maxLev))
+      ans = root;
 
-    // if both child-subtrees contain deepest-level nodes, this node is LCA
-    if (leftMax == maxi && rightMax == maxi) {
-      lowestRoot = root;
-    }
-
-    // single deepest leaf case
-    if (!root->left && !root->right && curr == maxi) {
-      lowestRoot = root;
-    }
-
-    return subtreeMax;
+    return max(l, r);
   }
 
   TreeNode *subtreeWithAllDeepest(TreeNode *root) {
-    searchMax(root, 0);
-    traverse(root);
-    return lowestRoot;
+    int maxLev = maxLevel(root);
+
+    cout << maxLev << endl;
+    recur(root, 0, maxLev);
+    return ans;
   }
 };

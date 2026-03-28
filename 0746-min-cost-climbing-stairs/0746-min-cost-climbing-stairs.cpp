f60a66b07e658340377a7c8306b198494ffb578d
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-03-28 08:25:28 +0600

    Time: 0 ms (100%), Space: 17.7 MB (50.77%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -6,14 +6,13 @@ public:
   int minCostClimbingStairs(vector<int> &cost) {
     int n = cost.size();
     vector<int> dp(n, INT_MAX);
-    // dp[0] = 0;
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 1; j < 3; j++) {
-        int prev = j > i ? 0 : dp[i - j];
-        dp[i] = min(dp[i], prev + cost[i]);
-      }
-    }
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+
+    for (int i = 2; i < n; i++)
+      dp[i] = min(dp[i], cost[i] + min(dp[i - 1], dp[i - 2]));
+
     return min(dp[n - 2], dp[n - 1]);
   }
 };

b05b99af1d026a66d12fa43d7227425fc8354b49
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-03-30 16:52:15 +0600

    Time: 49 ms (26.15%), Space: 128.4 MB (27.69%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
 class Solution {
 public:
   vector<int> successfulPairs(vector<int> &spells, vector<int> &potions,
-                              long long success) {
+                              ll success) {
     sort(potions.begin(), potions.end());
     int n = potions.size();
-    long long curr;
+    ll curr;
 
     vector<int> ans;
     for (auto x : spells) {
@@ -16,14 +17,14 @@ public:
       while (l < h) {
         int m = (l + h) / 2;
 
-        curr = (long long)x * (long long)potions[m];
+        curr = (ll)x * (ll)potions[m];
         if (curr < success)
           l = m + 1;
         else
           h = m;
       }
 
-      curr = (long long)x * (long long)potions[l];
+      curr = (ll)x * (ll)potions[l];
       if (l == n - 1 && curr < success)
         l++;
       ans.emplace_back(n - l);

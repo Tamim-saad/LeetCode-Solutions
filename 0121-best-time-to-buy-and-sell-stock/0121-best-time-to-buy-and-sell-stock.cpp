344e31d878df12e1dab6ec78fc47ca8e62a5b5bf
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-04-04 19:35:28 +0600

    Time: 6 ms (7.02%), Space: 97.2 MB (86.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,35 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
+  int maxProfit(vector<int> &prices) {
+    int inHand = 0;
+    int inHandAfterBuy = INT_MIN;
 
-int maximize_profit(int n,int arr[])
-{
-    int i,mid,max_profit=0;
-
-    int minSoFar=arr[0];
-
-    int left_sum=0;
-    int right_sum=0;
-
-
-    for(i=1; i<n; i++)
-    {
-        if(arr[i]>=minSoFar)
-        {
-            max_profit=max(max_profit,arr[i]-minSoFar);
-        }
-        else minSoFar=arr[i];
+    for (auto x : prices) {
+      inHandAfterBuy = max(inHandAfterBuy, -x);
+      inHand = max(inHand, inHandAfterBuy + x);
     }
 
-    return max_profit;
-}
-
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int arr[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=prices[i];
-        }
-        return maximize_profit(n,arr);
-    }
+    return inHand;
+  }
 };

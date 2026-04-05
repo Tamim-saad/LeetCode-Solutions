d43d911b1995de2b56fc1b30260a95efb22cc9bd
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-04-05 08:22:36 +0600

    Time: 4 ms (84.1%), Space: 23.3 MB (82.79%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  bool closeStrings(string word1, string word2) {
+    vector<int> f1(26, 0), f2(26, 0);
+
+    for (auto x : word1)
+      f1[x - 'a']++;
+    for (auto x : word2)
+      f2[x - 'a']++;
+
+    for (int i = 0; i < 26; i++)
+      if ((f1[i] == 0) != (f2[i] == 0))
+        return false;
+
+    sort(f1.begin(), f1.end());
+    sort(f2.begin(), f2.end());
+
+    for (int i = 0; i < 26; i++)
+      if (f1[i] != f2[i])
+        return false;
+
+    return true;
+  }
+};

f1579060803232922cf95cad03b77d9a62f740a9
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2026-08-05 09:18:18 +0600

    Time: 0 ms (100%), Space: 11.3 MB (10.75%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,23 +1,16 @@
-// say Alhamdulillah
-
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
   int rangeBitwiseAnd(int left, int right) {
-    if (left == right)
-      return left;
-    if (!left || !right)
-      return 0;
+    int i = 0;
+    while (left != right) {
+      left >>= 1;
+      right >>= 1;
+      i++;
+    }
 
-    long f = 1;
-    while (f <= left)
-      f <<= 1;
-    f >>= 1;
-
-    if (f <= left && left < (f * 2) && f <= right && right < (f * 2))
-      return f | (rangeBitwiseAnd(left - f, right - f));
-    return 0;
+    return left << i;
   }
 };
